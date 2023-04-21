f5b3385c86a5df7e709cf4fdb3893a915d828f0a
Nikita Glubokov <ITVenomancer@gmail.com>
2023-04-21 21:19:45 +0300

    Working on player health bars

---
+++ WarlocksPVP/Assets/Scripts/HealthBars.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBars : MonoBehaviour
+{
+    [SerializeField] private Slider _leftPlayerHealthBar;
+    [SerializeField] private Slider _rightPlayerHealthBar;
+    private int _leftHealthValue, _rightHealthValue = 15;
+
+
+
+    public static HealthBars Instance;
+    private void Awake()
+    {
+        Instance = this;
+
+        _leftPlayerHealthBar.value = _leftHealthValue;
+        _rightPlayerHealthBar.value = _rightHealthValue;
+    }
+}
